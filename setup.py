a19d8dcb3b9f5a38358d66cf91c79259cc47f39c
Tyler Kennedy <tk@tkte.ch>
2020-01-10 17:37:25 -0500

    Bugfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='flask-themer',
-    version='1.1.0',
+    version='1.1.1',
     description='Simple theme mechanism for Flask',
     author='Tyler Kennedy',
     author_email='tk@tkte.ch',
