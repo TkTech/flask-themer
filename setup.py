892df9db2cac06530ddb3650f416fef6fb535d5c
Tyler Kennedy <tk@tkte.ch>
2020-01-15 11:40:55 -0500

    Bump version: 1.1.1 → 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='flask-themer',
-    version='1.1.1',
+    version='1.2.0',
     description='Simple theme mechanism for Flask',
     author='Tyler Kennedy',
     author_email='tk@tkte.ch',
